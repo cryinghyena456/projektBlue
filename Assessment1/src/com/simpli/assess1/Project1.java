a7587cde204dc58f37e8053a3a4c88b8b9f8624b
cryinghyena <ghostmeropa@gmail.com>
2022-12-10 22:14:01 +0530

    Final done

--- Assessment1/src/com/simpli/assess1/Project1.java
+++ Assessment1/src/com/simpli/assess1/Project1.java
@@ -13,13 +13,17 @@ public class Project1 {
 		boolean tre = true;
 		while(tre) {
 Scanner sc = new Scanner(System.in);
-System.out.println("Hello! Welcome to XXX program");
+System.out.println("Hello! Welcome to LOCKEDME program");
+System.out.println("");
+System.out.println("Program created by: Trideep Deka");
+System.out.println("S/W Development Trainee at LOCKEDME.COM");
+System.out.println("");
 System.out.println("Please choose an option below");
-System.out.print("1: Create File");
-System.out.print("  2: Delete File");
-System.out.print("  3: Search File");
-System.out.print("  4: Sort Files");
-System.out.print("  5: Exit Program");
+System.out.println("  1: Sort Files");
+System.out.println("  2: Create A File");
+System.out.println("  3: Search A File");
+System.out.println("  4: Delete A File");
+System.out.println("  5: Exit The Program");
 
 
 int opt = sc.nextInt();
@@ -27,11 +31,27 @@ int opt = sc.nextInt();
 
 
 if(opt == 1) {
-	System.out.println("You have choosed to Create a File");
+	System.out.println("You have choosed to Sort the Files");
 	
+	
+
+	
+File fe = new File("D:\\Jawa");
+if(fe.isDirectory()) {
+List les = Arrays.asList(fe.list());
+System.out.println(les);
+Collections.sort(les);
+
+System.out.println(" ");
+}
+
+
+	
+	
+}else if(opt ==2) {
 	
 File f = new File("D:\\Jawa");
-	
+System.out.println("You have choosed to Create a File");
 	System.out.println("Please Enter file name: ");
 	
 	String ans = sc.next();
@@ -47,26 +67,8 @@ File f = new File("D:\\Jawa");
 	System.out.println(" ");
 	}else {
 		System.out.println("File already exists!");
+		System.out.println("");
 	}
-	
-	
-}else if(opt ==2) {
-	
-	System.out.println("You have choosed to delete a file");
-	System.out.println("Please Enter the Filename that you want to Delete: ");
-	
-	String 	del = sc.next();
-	File fe = new File("D:\\Jawa\\"+del);
-
-	boolean filedelete = fe.delete();
-	if(filedelete ==true) {
-
-System.out.print("File deleted");
-	}
-	else {System.out.println("File not found");
-	break;
-	}
-	
 	
 }else if (opt ==3) {
 	System.out.println("You have choosed to Search a File");
@@ -79,23 +81,34 @@ System.out.print("File deleted");
 	
 	if(contain == true) {
 		System.out.println("File with the name "+ entry+" Has been found");
-	}else {System.out.println("File not found");
-	break;}
+	}else {System.out.println("File not found. Please Try again");
+	System.out.println("");
+	}
 
 }else if(opt == 4) {
-	System.out.println("You have choosed to Sort the Files!");
-	File fe = new File("D:\\Jawa");
-	if(fe.isDirectory()) {
-	List les = Arrays.asList(fe.list());
-	System.out.println(les);
-	Collections.sort(les);
-	break;}
+	System.out.println("You have choosed to delete a file");
+	System.out.println("Please Enter the Filename that you want to Delete: ");
+		
+		String 	del = sc.next();
+		File fe = new File("D:\\Jawa\\"+del);
+
+		boolean filedelete = fe.delete();
+		if(filedelete ==true) {
+
+	System.out.print("File deleted");
+	System.out.println("");
+		}
+		else {System.out.println("File not found. Please Try again");
+		System.out.println("");
+		
+		}
 	
 }
 
 	else if (opt ==5) {
 		System.out.println("You have choosed to Exit the Program");
-		System.out.println("Exiting the program");
+		System.out.println("Thanks for using my program. Exiting the program now");
+		System.out.println("");
 		
 		tre = false;
 		break;
@@ -105,6 +118,7 @@ System.out.print("File deleted");
 	}
 else {
 	System.out.println("Invalid Entry. Please choose 1, 2, 3, 4");
+	System.out.println("");
 }
 		}
 	}
